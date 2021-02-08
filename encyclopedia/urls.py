60cfc5c2b2cd2016c0558160d005628a30a0fc4e
ni1985 <kir.nikel@gmail.com>
2021-02-08 22:37:48 +0100

    entries created

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.index, name="wiki"),
+    path("wiki/<str:entry_page>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,28 @@
+import markdown2
+
 from django.shortcuts import render
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, entry_page):
+
+    if entry_page in util.list_entries(): 
+
+        # Convert markdown text into HTML using the markdown module
+        text = markdown2.markdown(util.get_entry(entry_page))
+
+        return render(request, "encyclopedia/entry.html", {
+            "entry_text": text,
+            "entry_page": entry_page.capitalize()
+        })
+    
+    else:
+        return render(request, "encyclopedia/not_found.html")
+
+def search(request):
+    return render(request, "encyclopedia/search.html")
